e087346d8ed1f0eb31638615599cbb561813f32b
Saucistophe <carpentier.ch@gmail.com>
2025-02-17 13:00:25 +0100

    Added RSA unit test

--- src/test/java/org/saucistophe/increledger/logic/RsaCryptoServiceTest.java
+++ src/test/java/org/saucistophe/increledger/logic/RsaCryptoServiceTest.java
@@ -1,25 +1,41 @@
 package org.saucistophe.increledger.logic;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 import io.quarkus.test.junit.QuarkusTest;
 import io.quarkus.test.junit.TestProfile;
 import jakarta.inject.Inject;
+import jakarta.ws.rs.BadRequestException;
+import org.apache.commons.lang3.RandomStringUtils;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 @TestProfile(RsaCryptoProfile.class)
 @QuarkusTest
 class RsaCryptoServiceTest {
-  @Inject
-  CryptoService cryptoService;
+  @Inject CryptoService cryptoService;
 
   @Test
   void generateRSAKeyPair() {
     assertInstanceOf(RsaCryptoService.class, cryptoService);
     ((RsaCryptoService) cryptoService).generateRSAKeyPair();
-    var signature =cryptoService.sign("test");
-    assertTrue(signature.length()> 100);
+    var signature = cryptoService.sign("test");
+    assertTrue(signature.length() > 100);
     assertTrue(cryptoService.verify("test", signature));
-    }
+  }
 
+  @Test
+  void signAndVerify() {
+    var initialData = RandomStringUtils.insecure().nextAscii(100);
+    var signature = cryptoService.sign(initialData);
+    assertTrue(cryptoService.verify(initialData, signature));
+    assertFalse(cryptoService.verify(initialData + "a", signature));
+  }
+
+  @Test
+  void invalidSignature() {
+
+    assertThrows(
+        BadRequestException.class,
+        () -> cryptoService.verify("Whatever the data", "An in valid signature"));
+  }
 }
